72ae94e33acb7700f3a56967f402fabb0c6e3e8d
benschoenfeld <104653935+benschoenfeld@users.noreply.github.com>
2022-05-09 15:12:06 -0600

    End of day may 9

--- 02 Rollover Demo/sketch.js
+++ 02 Rollover Demo/sketch.js
@@ -24,12 +24,16 @@ function drawQuadrants(){
   fill(255);
   stroke(10);
   //top left
+  fill(fillTopLeft);
   rect(0,0, width/2, height/2);
   //top right
+  fill(fillTopRight);
   rect(width/2, 0, width/2, height/2);
   //bottom left
+  fill(fillBottomLeft);
   rect(0, height/2, width/2, height/2);
   //bottom right
+  fill(fillBottomRight);
   rect(width/2, height/2, width/2, height/2);
 
 }
@@ -45,11 +49,16 @@ if (mouseX > width / 2 && mouseY > height / 2) {
   fillBottomRight = 0;
 }
 //bottom left
-if (mouseX < width /2 && mouseY >= height/2){
+if (mouseX < width /2 && mouseY > height/2){
   fillBottomLeft = 0;
 }
-
-
-  
+//top right
+if (mouseX > width/2 && mouseY < height /2){
+  fillTopRight = 0;
+}
+//top left
+if (mouseX < width /2 && mouseY < height/2){
+  fillTopLeft = 0;
+}
 
 }
