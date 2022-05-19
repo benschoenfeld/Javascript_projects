638d50d2d248a974f6fe1beed2e7cf43c03deb29
benschoenfeld <104653935+benschoenfeld@users.noreply.github.com>
2022-05-19 12:08:02 -0600

    May  19th work period

--- Project 5 - Cars  Cars Cars/sketch.js
+++ Project 5 - Cars  Cars Cars/sketch.js
@@ -6,23 +6,75 @@
 // - describe what you did to take this project "above and beyond"
 
 
+let myCars;
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let col = color(random(255), random(255), random(255));
+
 }
 
 function draw() {
   background(255);
   drawRoad();
+  //myCars.display();
+  drawCar();
+  
 }
 
-function drawRoad(){
+function drawRoad() {
   noStroke();
   fill(0);
-  rect(0, height/4, width, height/2);
+  rect(0, height / 4, width, height / 2);
 
-  for (let x = 0; x < width; x += 50){
+  for (let x = 0; x < width; x += 50) {
     stroke(255);
-    strokeWeight(10);
-    line(x, windowHeight/2, x + 30, windowHeight/2);
+    strokeWeight(5);
+    line(x, windowHeight / 2, x + 30, windowHeight / 2);
   }
+}
+
+
+class Cars {
+  //class constructor (inc. properties)
+  constructor(x, y, diameter) {
+    this.x = x;
+    this.y = y;
+    //this.speed
+    this.size = 10;
+
+  }
+
+  // move() {
+
+
+  // }
+
+  display() {
+    rectMode(CENTER);
+    fill(this.c)
+    rect(this.x, this.y, this.size, this.size);
+
+
+  }
+
+}
+
+
+function drawCar() {
+noStroke();
+if(mouseButton === LEFT && mouseIsPressed) {
+   print("LEFT");
+    
+    fill(100);
+    rect(mouseX+5, mouseY-3, 10, 27);
+    rect(mouseX+30, mouseY-3, 10, 27);
+
+    fill(255, 0, 0);
+    rect(mouseX, mouseY, 45, 20);
+  
+}
+   
+
 }
