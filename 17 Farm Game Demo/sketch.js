aebed3ecba31c729b39d8475d2ef5c524a88b83c
benschoenfeld <104653935+benschoenfeld@users.noreply.github.com>
2022-06-02 14:57:39 -0600

    June 2nd work time

--- 17 Farm Game Demo/sketch.js
+++ 17 Farm Game Demo/sketch.js
@@ -14,19 +14,19 @@ let playerY = 3;
 
 
 let level = [
-[0, 0, 1, 0, 0],
-[0, 1, 0, 1, 0],
-[0, 0, 1, 0, 0],
-[0, 0, 0, 2, 0],
-[0, 0, 0, 0, 0] ];
+  [0, 0, 1, 0, 0],
+  [0, 1, 0, 1, 0],
+  [0, 0, 1, 0, 0],
+  [0, 1, 0, 1, 0],
+  [0, 0, 0, 0, 0]];
 
 let tiles = [];  //0 → blank
-                 //1 → chicken
-                 //2 → cow
+//1 → chicken
+//2 → cow
 
 function preload() {
-  for(let i = 0; i < 3; i++) {
-    tiles.push(loadImage("assets/"+i+".png"));
+  for (let i = 0; i < 3; i++) {
+    tiles.push(loadImage("assets/" + i + ".png"));
   }
 }
 
@@ -42,11 +42,11 @@ function draw() {
 
 function renderBoard() {
   //interpret data in our 2D array, and place images accordingly
-  for (let y = 0; y<ROWS; y++) {
-    for (let x = 0; x<COLUMNS; x++) {
+  for (let y = 0; y < ROWS; y++) {
+    for (let x = 0; x < COLUMNS; x++) {
       //(0, 0)  (0,1)  [0][2]  
       let imageIndex = level[y][x];
-      image(tiles[imageIndex], x*TILE_SIZE, y*TILE_SIZE);
+      image(tiles[imageIndex], x * TILE_SIZE, y * TILE_SIZE);
     }
   }
 }
@@ -56,28 +56,50 @@ function renderBoard() {
 function swap(x1, y1, x2, y2) {
   //simple manipulation of the gameboard; switch 2 adjacent items
   let temp = level[y1][x1];
-  level [y1][x1] = level[y2][x2];
+  level[y1][x1] = level[y2][x2];
   level[y2][x2] = temp;
 }
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-   swap(playerX, playerY, playerX, playerY - 1);
-   playerY--;
+    if (playerY > 0) {  //verify there is atleast one tile above the player
+      //if there is something there, let's check what it is:
+      if (level[playerY - 1][playerX] === 0) { // grass tile above
+        swap(playerX, playerY, playerX, playerY - 1);
+        playerY--;
+      } else if (level[playerY-1][playerX]===1) { //chicken tile above
+        if(playerY > 1 && level[playerY-2][playerX]===0) {
+          // indicates enough room to not go off the array
+          // and also that beyond the pushable object there is grass tile
+          swap(playerX, playerY-1, playerX, playerY-2);
+          swap(playerX, playerY, playerX, playerY-1);
+          playerY--;
+        }
+      }
+
+
+
+    }
   }
 
   if (keyCode === DOWN_ARROW) {
-    swap(playerX, playerY, playerX, playerY +1);
-    playerY++;
+    if (playerY < 4) {
+      swap(playerX, playerY, playerX, playerY + 1);
+      playerY++;
+    }
   }
 
   if (keyCode === LEFT_ARROW) {
-    swap(playerX, playerY, playerX -1, playerY);
-    playerX--;
+    if (playerX > 0) {
+      swap(playerX, playerY, playerX - 1, playerY);
+      playerX--;
+    }
   }
 
   if (keyCode === RIGHT_ARROW) {
-    swap(playerX, playerY, playerX +1, playerY);
-    playerX++;
+    if (playerX < 4) {
+      swap(playerX, playerY, playerX + 1, playerY);
+      playerX++;
+    }
   }
 }
--- Project 7 - Puzzle Game/sketch.js
+++ Project 7 - Puzzle Game/sketch.js
@@ -2,75 +2,104 @@
 // Ben Schoenfeld
 // June 1, 2022
 
-//Create a puzzle game that...
+// Create a puzzle game where a random checkered-style grid is generated
+// and must be changed to either all white or all black with a secret cheat
 
 
 let NUM_ROWS = 4;
 let NUM_COLS = 5;
 let rectWidth, rectHeight;
 let currentRow, currentCol;
-let gridData = [[0,0,0,0,0],
-                [0,0,0,0,0],
-                [0,255,0,0,0],
-                [255,255,255,0,0]];
-
+let gridData = [[0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0],
+                [0, 255, 0, 0, 0],
+                [255, 255, 255, 0, 0]];
+let randomCol = [0, 255];
 
 
 function setup() {
-  // Determine the size of each square. Could use windowHeight,windowHeight  for Canvas to keep a square aspect ratio
+  // Determine the size of each square. Could use windowHeight,windowHeight  
+  // for Canvas to keep a square aspect ratio
   createCanvas(windowWidth, windowHeight);
-  rectWidth = width/NUM_COLS;
-  rectHeight = height/NUM_ROWS;
+  rectWidth = width / NUM_COLS;
+  rectHeight = height / NUM_ROWS;
+  randomizer();
+
+  
 }
-
-function draw() {
-  background(220);
-  determineActiveSquare();   //figure out which tile the mouse cursor is over
-  drawGrid();                //render the current game board to the screen (and the overlay)
-}
-
-
-
-function mousePressed(){
-  // cross-shaped pattern flips on a mouseclick. Boundary conditions are checked within the flip function to ensure in-bounds access for array
-  // CHEAT: use shift-left click to flip a single square
-
-if (mouseButton === LEFT && keyIsDown(16) && keyIsPressed) {  
-  print ("hello");
-  flip(currentCol, currentRow);
-  } else {
-    flip(currentCol, currentRow);
-    flip(currentCol-1, currentRow);
-    flip(currentCol+1, currentRow);
-    flip(currentCol, currentRow-1);
-    flip(currentCol, currentRow+1);
-
+  function draw() {
+    background(220);
+    determineActiveSquare();   //figure out which tile the mouse cursor is over
+    drawGrid();                //render the current game board to the screen (and the overlay)
+    win();               
   }
-}
-function flip(col, row){
-  // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
-  // conditions ensure that the col and row given are valid and exist for the array. If not, no operations take place.
-  if (col >= 0 && col < NUM_COLS ){
-    if (row >= 0 && row < NUM_ROWS){
-      if (gridData[row][col] === 0) gridData[row][col] = 255;
-      else gridData[row][col] = 0;
+
+
+
+  function mousePressed() {
+    // cross-shaped pattern flips on a mouseclick. Boundary conditions are checked 
+    // within the flip function to ensure in-bounds access for array
+    // CHEAT: use shift-left click to flip a single square
+
+    if (mouseButton === LEFT && keyIsDown(16) && keyIsPressed) {
+      flip(currentCol, currentRow);
+    } else {
+      flip(currentCol, currentRow);
+      flip(currentCol - 1, currentRow);
+      flip(currentCol + 1, currentRow);
+      flip(currentCol, currentRow - 1);
+      flip(currentCol, currentRow + 1);
+
     }
   }
-}
-
-function determineActiveSquare(){
-  // An expression to run each frame to determine where the mouse currently is.
-  currentRow = int(mouseY / rectHeight);
-  currentCol = int(mouseX / rectWidth);
-}
-
-function drawGrid(){
-  // Render a grid of squares - fill color set according to data stored in the 2D array
-  for (let x = 0; x < NUM_COLS ; x++){
-    for (let y = 0; y < NUM_ROWS; y++){
-      fill(gridData[y][x]); 
-      rect(x*rectWidth, y*rectHeight, rectWidth, rectHeight);
+  function flip(col, row) {
+    // given a column and row for the 2D array, flip its value from 0 to 255 or 255 to 0
+    // conditions ensure that the col and row given are valid and exist for the array. If not, no operations take place.
+    if (col >= 0 && col < NUM_COLS) {
+      if (row >= 0 && row < NUM_ROWS) {
+        if (gridData[row][col] === 0) gridData[row][col] = 255;
+        else gridData[row][col] = 0;
+      }
     }
   }
-}
 
+  function determineActiveSquare() {
+    // An expression to run each frame to determine where the mouse currently is.
+    currentRow = int(mouseY / rectHeight);
+    currentCol = int(mouseX / rectWidth);
+  }
+
+  function drawGrid() {
+    // Render a grid of squares - fill color set according to data stored in the 2D array
+    for (let x = 0; x < NUM_COLS; x++) {
+      for (let y = 0; y < NUM_ROWS; y++) {
+        fill(gridData[y][x]);
+        rect(x * rectWidth, y * rectHeight, rectWidth, rectHeight);
+      }
+    }
+  }
+
+
+function win() { //display text "you win" to represent completion
+    for (let x = 0; x < NUM_COLS; x++) {
+      for (let y = 0; y < NUM_ROWS; y++) {
+        if (gridData[0][0] !== gridData[y][x]) {
+          return;
+      }
+    }
+  }
+    textAlign(CENTER,CENTER);
+    fill(random(255), random(255), random(255));
+    textSize(32);
+    text('YOU WIN', width / 2, height / 2);
+
+}
+//randomizes the tiles in setup of canvas
+function randomizer() {
+ for (let x = 0; x < NUM_COLS; x++) {
+      for (let y = 0; y < NUM_ROWS; y++) {
+        gridData[y][x] = (int(random(0,2))*255);
+        
+      }
+    }
+}
